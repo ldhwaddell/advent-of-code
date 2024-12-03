519cdda06cfdede526ffc75a25b343ba67035507
Lucas Waddell <ldhwaddell@gmail.com>
2024-12-02 20:00:33 -0400

    Day 2 - Part 2

--- 2024/02/main.py
+++ 2024/02/main.py
@@ -3,22 +3,27 @@ from typing import List
 
 
 def validate(report: List[int]) -> bool:
-    if all(i < j for i, j in zip(report, report[1:])) or all(
-        i > j for i, j in zip(report, report[1:])
-    ):
-        for i, j in zip(report, report[1:]):
-            diff = abs(i - j)
-            if diff < 1 or diff > 3:
-                return False
+    increasing = all(i < j for i, j in zip(report, report[1:]))
+    decreasing = all(i > j for i, j in zip(report, report[1:]))
+    valid_diff = all(1 <= abs(i - j) <= 3 for i, j in zip(report, report[1:]))
+
+    if (increasing and valid_diff) or (decreasing and valid_diff):
         return True
     return False
 
 
-safe_report = 0
+safe_report_part_1 = 0
+safe_report_part_2 = 0
 for line in fileinput.input():
     report = [int(n) for n in line.split()]
+
     if validate(report):
-        safe_report += 1
+        safe_report_part_1 += 1
+
+    # Check all sublists
+    if any(validate(report[:i] + report[i + 1 :]) for i in range(len(report))):
+        safe_report_part_2 += 1
 
 
-print(f"Part 1: {safe_report}")
+print(f"Part 1: {safe_report_part_1}")
+print(f"Part 2: {safe_report_part_2}")
