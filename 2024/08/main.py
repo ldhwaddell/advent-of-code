830d6338876a8a4a9b6ee95fdc5966f101720743
Lucas Waddell <ldhwaddell@gmail.com>
2024-12-08 23:27:24 -0400

    Day 8 - Part 2

--- 2024/08/main.py
+++ 2024/08/main.py
@@ -48,4 +48,38 @@ for symbol, coords in symbols.items():
         if is_valid_point(antinode2_row, antinode2_col):
             antinodes.add((antinode2_row, antinode2_col))
 
+
 print(f"Part 1: {len(antinodes)}")
+
+## Part 2 ##
+
+antinodes = set()
+for symbol, coords in symbols.items():
+    if len(coords) < 2:
+        continue
+
+    # Get all pairs of antennas with the same frequency
+    for (ax, ay), (bx, by) in combinations(coords, 2):
+        # Add current pair as they now count as antinodes
+        antinodes.add((ax, ay))
+        antinodes.add((bx, by))
+
+        # Compute difference between antennas
+        delta_row = bx - ax
+        delta_col = by - ay
+
+        # Just pick one of the points. While - delta makes valid point, add.
+        # While + delta makes valid point, add.
+        above_row, above_col = (ax - delta_row, ay - delta_col)
+        below_row, below_col = (ax + delta_row, ay + delta_col)
+
+        while is_valid_point(above_row, above_col):
+            antinodes.add((above_row, above_col))
+            above_row, above_col = (above_row - delta_row, above_col - delta_col)
+
+        while is_valid_point(below_row, below_col):
+            antinodes.add((below_row, below_col))
+            below_row, below_col = (below_row + delta_row, below_col + delta_col)
+
+
+print(f"Part 2: {len(antinodes)}")
