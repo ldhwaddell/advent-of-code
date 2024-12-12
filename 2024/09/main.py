f9c3bebb9ef6ed67bf1a405c8315391cfee075ab
Lucas Waddell <ldhwaddell@gmail.com>
2024-12-11 22:43:36 -0400

    Day 10 - Part 1

--- 2024/09/main.py
+++ 2024/09/main.py
@@ -1,50 +1,44 @@
 import fileinput
 
-
-def parse_input(input_line):
-    blocks = []
-    curr_id = 0
-    for i in range(len(input_line)):
-        count = int(input_line[i])
-        if i % 2 == 0:
-            blocks.extend([curr_id] * count)
-            curr_id += 1
-        else:
-            blocks.extend(["."] * count)
-    return blocks
+grid = []
+for line in fileinput.input():
+    row = list(map(int, [n for n in line.strip()]))
+    grid.append(row)
 
 
-def rearrange_blocks_1(blocks):
-    l, r = 0, len(blocks) - 1
-    while l < r:
-        if blocks[l] != ".":
-            l += 1
-        elif blocks[r] == ".":
-            r -= 1
-        else:
-            blocks[l], blocks[r] = blocks[r], blocks[l]
-            l += 1
-            r -= 1
-    return blocks
+rows, cols = len(grid), len(grid[0])
 
 
-def calculate_score(blocks):
-    return sum(i * block for i, block in enumerate(blocks) if block != ".")
+def search(r, c, target, nines):
+    # If target is 10, we are currently on a 9. Add it and return from fn.
+    if target == 10:
+        nines.add((r, c))
+        return
+
+    # Check up, down, left, right
+    dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    for dr, dc in dirs:
+        nr, nc = r + dr, c + dc
+
+        if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == target:
+            # Continue search from here
+            search(nr, nc, target + 1, nines)
 
 
-input_line = [line.strip() for line in fileinput.input()][0]
-blocks = parse_input(input_line)
-blocks = rearrange_blocks_1(blocks)
-part_1 = calculate_score(blocks)
+part_1 = 0
+for r in range(rows):
+    for c in range(cols):
+        if grid[r][c] == 0:
+            # Create a set to keep track of the nines for this 0
+            nine_set = set()
+
+            # Search for all 9's reachable from this 0
+            # Search
+            search(r, c, 1, nine_set)
+
+            # Add the # of results to part_1
+            part_1 += len(nine_set)
+
+
 print(f"Part 1: {part_1}")
-
-
-def rearrange_blocks_contiguous(blocks):
-    return blocks
-
-
-input_line = [line.strip() for line in fileinput.input()][0]
-blocks = parse_input(input_line)
-blocks = rearrange_blocks_contiguous(blocks)
-part_2 = calculate_score(blocks)
-print(f"Part 2: {part_2}")
