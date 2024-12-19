8f9fd9988686246c68bc42817d211d40be96b9e8
Lucas Waddell <ldhwaddell@gmail.com>
2024-12-18 22:52:05 -0400

    Day 18 - Part 2

--- 2024/18/main.py
+++ 2024/18/main.py
@@ -17,6 +17,8 @@ def bfs(num_coords, start, end):
     # Keep track of the # of steps and current coords
     steps = [(0, (0, 0))]
 
+    # Keeps track of each possible path, and the distance to that point
+    # When we reach the end, the first set of coords to do so will be shortest path.
     for dist, (row, col) in steps:
         if (row, col) == end:
             return dist
@@ -40,3 +42,19 @@ def bfs(num_coords, start, end):
 
 part_1 = bfs(1024, (0, 0), (ROWS, COLUMNS))
 print(f"Part 1: {part_1}")
+
+
+# We know at least the first 1024 bytes mean it is still possible to find end...
+num_bytes = 1024
+
+while True:
+    print(f"Checking first {num_bytes} bytes")
+    path = bfs(num_bytes, (0, 0), (ROWS, COLUMNS))
+    if not path:
+        # Lists are 0-indexed!!
+        breaking_coords = corrupted_coords[num_bytes - 1]
+        print(f"Part 2: {breaking_coords}")
+        break
+
+    else:
+        num_bytes += 1
